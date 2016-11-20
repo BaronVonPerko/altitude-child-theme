50155fd27b102789e1d3ae7695a215615c8e8b81
Chris Perko <cperko@teachers-teachers.com>
2016-11-20 16:00:40 -0600

    Custom post type for portfolio. Display portfolio items on portfolio page

---
+++ content.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Content Template
+ *
+ */
+?>
+<?php the_category(true); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="article-container xs-col-12">
+
+		<header class="entry-header">
+			<div class="entry-date"><?php altitude_posted_on(); ?></div>
+			<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+			<div class="post-comments"><?php altitude_comment_meta(); ?></div>
+		</header><!-- .entry-header -->
+
+		<div class="entry-summary">
+			<div class="entry-thumbnail"><?php the_post_thumbnail( 'altitude-blog' ); ?></div>
+			<div class="entry-excerpt"><?php the_excerpt(); ?></div>
+		</div><!-- .entry-summary -->
+
+		<footer class="entry-footer">
+			<div class="entry-edit"><?php edit_post_link( __( 'Edit', 'altitude' ), '<span class="edit-link">', '</span>' ); ?></div>
+		</footer><!-- .entry-footer -->
+
+	</div><!-- article-container -->
+</article><!-- #post-## -->
--- functions.php
+++ functions.php
@@ -4,3 +4,18 @@ add_action( 'wp_enqueue_scripts', 'latte_child_enqueue_styles' );
 function latte_child_enqueue_styles() {
   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+
+add_action( 'init', 'create_portfolio_post_type' );
+function create_portfolio_post_type() {
+  register_post_type( 'portfolio',
+    array(
+      'labels' => array(
+        'name' => __( 'Portfolio Items' ),
+        'singular_name' => __( 'Portfolio Item' )
+      ),
+      'public' => true,
+      'has_archive' => true
+    )
+  );
+}
+add_post_type_support( 'portfolio', 'thumbnail' );
