e2a03bf167571fd556cbffab1fa74ade5bfcc930
Chris Perko <cperko@teachers-teachers.com>
2016-11-25 13:36:04 -0600

    Added titles to portfolio entires

--- inc/templates/portfolio.php
+++ inc/templates/portfolio.php
@@ -4,6 +4,7 @@
 		<?php $query = new WP_Query( array( 'post_type' => 'portfolio' ) ); ?>
 		<?php while( $query->have_posts() ) : $query->the_post(); ?>
 
+			<h1><?php echo the_title(); ?></h1>
 			<?php echo get_the_post_thumbnail(); ?>
 			<div class="portfolio-content">
 				<?php echo the_content(); ?>
