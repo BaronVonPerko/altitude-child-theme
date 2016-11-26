ccbab395bf4b304f7cc73ce5b06c207fc3dc4030
Chris Perko <cperko@teachers-teachers.com>
2016-11-25 21:55:48 -0600

    Final cleanup of portfolio

--- inc/templates/portfolio.php
+++ inc/templates/portfolio.php
@@ -4,7 +4,7 @@
 		<?php $query = new WP_Query( array( 'post_type' => 'portfolio' ) ); ?>
 		<?php while( $query->have_posts() ) : $query->the_post(); ?>
 
-			<h1><?php echo the_title(); ?></h1>
+			<h3><?php echo the_title(); ?></h3>
 			<?php echo get_the_post_thumbnail(); ?>
 			<div class="portfolio-content">
 				<?php echo the_content(); ?>
