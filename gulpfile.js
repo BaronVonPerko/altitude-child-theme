ac891ffe7d621592bc9a4060bf592629edefa18c
Chris Perko <cperko@teachers-teachers.com>
2017-01-25 15:03:47 -0600

    Minifed custom css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp        = require("gulp"),
-    browserSync = require("browser-sync");
+    browserSync = require("browser-sync"),
+    concatCss   = require("gulp-concat-css"),
+    cleanCss    = require("gulp-clean-css");
     
     
 gulp.task('sync', function() {
@@ -7,7 +9,18 @@ gulp.task('sync', function() {
     
     browserSync.init(files, {
         port: 8082, // c9.io only
-        // injectChanges: true,
+        injectChanges: true,
         notify: false
     });
+});
+
+
+gulp.task('css', function() {
+    gulp.src('inc/**/*.css')
+        .pipe(concatCss('custom.css'))
+        .pipe(gulp.dest('inc/css/'));
+
+    return gulp.src('inc/css/custom.css')
+        .pipe(cleanCss({compatibility: 'ie8'}))
+        .pipe(gulp.dest('dist'));
 });
